a65f238b2e4dc6a51363d1a9ec9a6a7b6fcc8ed5
morishitter <morishitter@gmail.com>
2016-10-23 18:22:24 +0900

    change separator from one space to ','

--- index.js
+++ index.js
@@ -3,10 +3,10 @@ var postcss = require('postcss')
 module.exports = postcss.plugin('postcss-ref', function () {
     return function (root) {
         root.walkAtRules('ref', function (atrule) {
-            var params = atrule.params.split(' ')
-            var selector = params[0]
-            var refedProperty = params[1]
-            var newProperty = params.length === 3 ? params[2] : refedProperty
+            var params = atrule.params.split(',')
+            var selector = params[0].trim()
+            var refedProperty = params[1].trim()
+            var newProperty = params.length === 3 ? params[2].trim() : refedProperty
 
             var newValue
             root.walkRules(selector, function (rule) {
