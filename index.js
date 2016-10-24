acd66f72e6db79d8924fd463c56b1eead8fe0435
Dan Gamble <dangamble89@gmail.com>
2016-10-24 13:09:26 +0100

    Add a function based way to use `ref`, Ref #2

--- index.js
+++ index.js
@@ -1,40 +1,62 @@
 var postcss = require('postcss')
 
-module.exports = postcss.plugin('postcss-ref', function () {
-    return function (root) {
-        root.walkAtRules('ref', function (atrule) {
-            var params = atrule.params.split(',')
-            var selector = params[0].trim()
-            var refedProperty = params[1].trim()
-            var newProperty = params.length === 3 ? params[2].trim() : refedProperty
-            var newValue = lookUpProperty(root, selector, refedProperty)
+module.exports = postcss.plugin('postcss-ref', function (opts) {
+    opts = opts || { atRule: true }
 
-            replaceAtRef(atrule, {
-                prop: newProperty,
-                value: newValue
+    return function (root) {
+        if (opts.atRule) {
+            root.walkAtRules('ref', function (atrule) {
+                var params = atrule.params.split(',')
+                var selector = params[0].trim()
+                var refedProperty = params[1].trim()
+                var newProperty = params.length === 3 ? params[2].trim() : refedProperty
+
+                var newValue
+                root.walkRules(selector, function (rule) {
+                    rule.walkDecls(refedProperty, function (decl) {
+                        newValue = decl.value
+                        if (isCustomProperty(refedProperty)) {
+                            newValue = 'var(' + refedProperty + ')'
+                        }
+                    })
+                })
+
+                if (atrule.parent.type === 'rule') {
+                    atrule.parent.insertBefore(atrule, {
+                        prop: newProperty,
+                        value: newValue
+                    })
+                }
+
+                atrule.remove()
             })
-        })
+        } else {
+            root.walkRules(function (rule) {
+                rule.walkDecls(function (decl) {
+                    if (decl.value.indexOf('ref(') !== -1) {
+                        var match = decl.value.match(/ref\((.*), (.*)\)/)
+                        var selector = match[1]
+                        var refedProperty = match[2]
+
+                        var newValue
+
+                        root.walkRules(selector, function (rule) {
+                            rule.walkDecls(refedProperty, function (decl) {
+                                newValue = decl.value
+
+                                if (isCustomProperty(refedProperty)) newValue = 'var(' + refedProperty + ')'
+                            })
+                        })
+
+                        decl.value = newValue
+                    }
+                })
+            })
+
+        }
     }
 })
 
-function isCustomProperty (property) {
+function isCustomProperty(property) {
     return /^--/.test(property)
-}
-
-function lookUpProperty (root, selector, refedProperty) {
-    var ret
-    root.walkRules(selector, function (rule) {
-        rule.walkDecls(refedProperty, function (decl) {
-            ret = isCustomProperty(refedProperty) ? ret = 'var(' + refedProperty + ')' : decl.value
-        })
-    })
-
-    return ret
-}
-
-function replaceAtRef (atrule, newDecl) {
-    if (atrule.parent.type === 'rule') {
-        atrule.parent.insertBefore(atrule, newDecl)
-    }
-    atrule.remove()
 }
